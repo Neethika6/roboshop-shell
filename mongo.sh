be7339496bf106e5b295bf55d113bc0364e30cc2
Neethika6 <neethikadakoju346@gmail.com>
2025-06-27 12:14:04 +0530

    ROBOSHOP

--- mongo.sh
+++ mongo.sh
@@ -6,39 +6,39 @@ echo $SCRIPT_NAME
 LOG_FILE=$LOG_FOLDER/$SCRIPT_NAME.log
 echo $LOG_FILE
 mkdir -p $LOG_FOLDER
-echo "Script started at:$(date)"
+echo "Script started at:$(date)" | tee -a $LOG_FILE
 
 if [ $USER == 0 ]
 then
-    echo "YOU ARE IN ROOT"
+    echo "YOU ARE IN ROOT" | tee -a $LOG_FILE
 else
-    echo "ERROR:PLEASE SWITCH TO ROOT"
+    echo "ERROR:PLEASE SWITCH TO ROOT" | tee -a $LOG_FILE
 fi
 
 VALIDATE()
 {
     if [ $1 == 0 ]
     then
-        echo "$2 is SUCCESS"
+        echo "$2 is SUCCESS" | tee -a $LOG_FILE
     else
-        echo "$2 is FAILED"
+        echo "$2 is FAILED" | tee -a $LOG_FILE
     fi
 }
 
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Copying mongodb repo to the repository directory"
 
-dnf install mongodb-org -y
+dnf install mongodb-org -y &>>$LOG_FILE
 VALIDATE $? "Installing mongodb"
 
-systemctl enable mongod
+systemctl enable mongod &>>$LOG_FILE
 VALIDATE $? "Enabling mongodb"
 
-systemctl start mongod
+systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "Starting mongodb"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Updating the IP of mongodb"
 
-systemctl restart mongod
+systemctl restart mongod &>>$LOG_FILE
 VALIDATE $? "Restarting mongodb"
