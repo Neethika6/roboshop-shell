2c252aed46d16f3c11564f099abb349dffe30afd
Neethika6 <neethikadakoju346@gmail.com>
2025-06-27 11:07:08 +0530

    ROBOSHOP

---
+++ mongo.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USER=$(id -u)
+PATH=$PWD
+echo $PATH
+
+if [ $USER == 0 ]
+then
+    echo "YOU ARE ROOT USER"
+else
+    echo "ERROR::PLEASE SIWTCH TO ROOT"
+    exit 1
+done
+
+VALIDATE()
+{
+    if [ $1 == 0 ]
+    then
+        echo "$2..SUCCESS"
+    else
+        echo "$2..FAILED"
+    fi
+}
+
+cp $PATH/mongo.sh /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Copying mongodb repo to the repositories path"
