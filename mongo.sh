9009c4e79bcbcf9ff8aa5a9b16f84273e8d8235a
Neethika6 <neethikadakoju346@gmail.com>
2025-06-27 11:38:39 +0530

    ROBOSHOP

--- mongo.sh
+++ mongo.sh
@@ -1,26 +1,37 @@
 #!/bin/bash
-
 USER=$(id -u)
-# PATH=$PWD
-# echo $PATH
 
 if [ $USER == 0 ]
 then
-    echo "YOU ARE ROOT USER"
+    echo "YOU ARE IN ROOT"
 else
-    echo "ERROR::PLEASE SIWTCH TO ROOT"
-    exit 1
+    echo "ERROR:PLEASE SWITCH TO ROOT"
 fi
 
 VALIDATE()
 {
     if [ $1 == 0 ]
     then
-        echo "$2..SUCCESS"
+        echo "$2 is SUCCESS"
     else
-        echo "$2..FAILED"
+        echo "$2 is FAILED"
     fi
 }
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? "Copying mongodb repo to the repositories path"
+cp mongo.rep /etc/yum.d.repos/mongo.repo
+VALIDATE $? "Copying mongodb repo to the repository directory"
+
+dnf install mongodb-org -y
+VALIDATE $? "Installing mongodb"
+
+systemctl enable mongodb
+VALIDATE $? "Enabling mongodb"
+
+systemctl start mongodb
+VALIDATE $? "Starting mongodb"
+
+sed -i 's/0.0.0.0/127.0.0.1/g' /etc/mongo.conf
+VALIDATE "Updating the IP of mongodb"
+
+systemctl restart mongodb
+VALDIATE $? "Restarting mongodb"
