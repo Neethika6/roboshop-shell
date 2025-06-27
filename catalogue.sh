44b6de5bbd2781389f542f9088227a987bec920f
Neethika6 <neethikadakoju346@gmail.com>
2025-06-27 14:08:46 +0530

    ROBOSHOP

--- catalogue.sh
+++ catalogue.sh
@@ -6,7 +6,7 @@ LOG_DIR="/var/log/roboshop"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 echo $SCRIPT_NAME
 LOG_FILE="$LOG_DIR/$SCRIPT_NAME.log"
-mkidr -p $LOG_DIR
+mkdir -p $LOG_DIR
 
 echo "SCRIPT START TIME IS:$(date)"
 
