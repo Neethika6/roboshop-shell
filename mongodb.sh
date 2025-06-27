6298936a3ee6838771e921af40850db9d8bc25aa
Neethika6 <neethikadakoju346@gmail.com>
2025-06-27 08:27:00 +0530

    ROBOSHOP EC2 INSTANCE CREATION

--- mongodb.sh
+++ mongodb.sh
@@ -10,6 +10,16 @@ else
     exit 1
 fi
 
+VALIDATE()
+{
+    if [ $1 == 0 ]
+    then
+        echo "$2 Installation is SUCCESS"
+    else
+        echo "$2 Installation FAILED"
+    fi
+}
+
 dnf list installed mongodb
 
 if [ $? == 0 ]
@@ -17,4 +27,6 @@ then
     echo "MONGODB is already installed"
 else
     echo "MONGODB is not Installed.Please proceed with Installation"
+    dnf install mongodb
+    VALIDATE $? "MONGODB"
 fi
