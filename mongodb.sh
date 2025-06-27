1c3aca0b15239943747727f186abfd28acf26008
Neethika6 <neethikadakoju346@gmail.com>
2025-06-27 08:54:32 +0530

    ROBOSHOP EC2 INSTANCE CREATION

--- mongodb.sh
+++ mongodb.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 ID=$(id -u)
+PATH=$PWD
+echo $PATH
+LOGS_FOLDER=/var/log/roboshop
+SCRIPT=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FILDER/$SCRIPT.log"
+mkdir -p $LOGS_FOLDER
 
 if [ $ID == 0 ]
 then
-    echo "You are in ROOT"
+    echo "You are in ROOT" | tee -a $LOG_FILE
 else
-    echo "ERROR:Please switch to ROOT"
+    echo "ERROR:Please switch to ROOT" | tee -a $LOG_FILE
     exit 1
 fi
 
@@ -14,19 +20,28 @@ VALIDATE()
 {
     if [ $1 == 0 ]
     then
-        echo "$2 Installation is SUCCESS"
+        echo "$2   SUCCESS" | tee -a $LOG_FILE
     else
-        echo "$2 Installation FAILED"
+        echo "$2 FAILED" | tee 0a $LOG_FILE
     fi
 }
 
-dnf list installed mongodb
 
-if [ $? == 0 ]
-then
-    echo "MONGODB is already installed"
-else
-    echo "MONGODB is not Installed.Please proceed with Installation"
-    dnf install mongodb
-    VALIDATE $? "MONGODB"
-fi
+cp $PATH/mongodb.repo /etc/yum.d.repo/mongodb.repo
+VALIDATE $? "Copying mongodb repo file to repository directory"
+
+dnf install mongodb-org -y 
+VALIDATE $? "Installing MONGODB"
+
+systemctl enable mongod 
+VALIDATE $? "Enabling MONGODB"
+
+systemctl start mongod
+VALIDATE $? "Starting MONGODB"
+
+sed -i "s/0.0.0.0/127.0.0.1/g' /etc/mongod.conf
+VALIDATE $? "Updating IP in MONGODB conf"
+
+systemctl restart mongod
+VALIDATE $? "RESTARTING MONGODB"
+
