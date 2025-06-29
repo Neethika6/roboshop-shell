d791fd8915b3adc7f6da146326aaf91128db06ff
Neethika6 <neethikadakoju346@gmail.com>
2025-06-29 15:31:37 +0530

    ROBOSHOP

--- redis.sh
+++ redis.sh
@@ -33,13 +33,13 @@ VALIDATE()
 }
 
 #Redis setup
-dnf module disable redis -y
+dnf module disable redis -y &>>$LOG_FILE
 VALIDATE $? "Disbale the default redis"
 
-dnf module enable redis:7 -y
+dnf module enable redis:7 -y &>>$LOG_FILE
 VALIDATE $? "Enable redis version:7"
 
-dnf install redis -y 
+dnf install redis -y &>>$LOG_FILE
 VALIDATE $? "Install redis version:7"
 
 sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
