f580d91fbabf26f6f2e0c23a7f44b14047cf1aa9
Neethika6 <neethikadakoju346@gmail.com>
2025-06-29 13:17:07 +0530

    ROBOSHOP

--- frontend.sh
+++ frontend.sh
@@ -1,59 +1,66 @@
 #!/bin/bash
 
-USER_ID=$(id -u)
-LOGS_PATH="/var/log/roboshop"
-SCRIP_NAME=$(echo $0 | cut -d "." -f1)
-LOG_FILE="$LOGS_PATH/$SCRIP_NAME.log"
-mkdir -p $LOGS_PATH
-SCRIP_PATH=$PWD
+START_TIME=$(date +%s)
+echo "Script Started at:$(date)"
+#Check if you are running with root or not
+USER_ID=$(id -u) #if the value is 0 then you are running with root 
+SCRIPT_DIR=$PWD
+LOG_DIR="/var/log/roboshop_logs"
+SCRIPT_NAME="$(echo $0 | cut -d "." -f1)"
+LOG_FILE="$LOG_DIR/$SCRIPT_NAME.log"
 
-echo "Script started at:$(date)" | tee -a $LOG_FILE
+#Creates a dir if not present -p will not throw error if the dir is already present
+mkdir -p /var/log/roboshop_logs
 
+#if the value is 0 then you are running with root
 if [ $USER_ID == 0 ]
-then 
+then
     echo "YOU ARE IN ROOT" | tee -a $LOG_FILE
 else
-    echo "ERROR:PLEASE SWITCH TO ROOT" |  tee
-    exit 1
+    echo "ERROR:PLEASE SWITCH TO ROOT" | tee -a $LOG_FILE
+    exit 1  #Script will not execute any lines if you are not in the ROOT
 fi
 
+#Function to check if the commands were executed correctly or not
 VALIDATE()
 {
     if [ $1 == 0 ]
-    then
-        echo "$2 is SUCCESS"
+    then 
+        echo "$2...SUCCESS" | tee -a $LOG_FILE
     else
-        echo "$2 FAILED"
-        exit 1
+        echo "$2...Failed" | tee -a $LOG_FILE
     fi
 }
 
-dnf module list nginx
-VALIDATE $? "Checking if NGINX is already installed"
+#NGINX SETUP
 
-dnf module disable nginx -y
-VALIDATE $? "Disabling already installed NGINX"
+dnf module disable nginx -y &>>$LOG_FILE
+VALIDATE $? "Disabling default nginx"
 
-dnf module enable nginx:1.24 -y
-VALIDATE $? "Enabling nginx of version 24"
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
+VALIDATE $? "Enabling nginx version:1.24"
 
-dnf install nginx -y
-VALIDATE $? "Installing NGINX"
+dnf isntall nginx -y &>>$LOG_FILE
+VALIDATE $? "Installing nginx"
 
 systemctl enable nginx
-VALIDATE $? "Enabling nginx"
-
 systemctl start nginx
-VALIDATE $? "Starting NGINX"
+VALIDATE $? "Enabling and starting nginx"
 
 rm -rf /usr/share/nginx/html/*
-VALIDATE $? "Removing default file in html folder"
-
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip 
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
-VALIDATE $? "Unzipping files is done"
+VALIDATE $? "Unzipping frontend.zip"
 
-cp $SCRIP_PATH/nginx.conf /usr/share/nginx/html/nginx.conf
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "Copying nginx conf to the configuration path"
+
 systemctl restart nginx
-VALIDATE $? "Restarted NGINX"
+VALIDATE $? "Restarting NGINX"
+
+
+END_TIME=$(date +%s)
+echo "Script Completed at:$(date)"
+TOTAL_TIME=$(($END_TIME - $START_TIME))
+echo "Total time taken: $TOTAL_TIME seconds"
