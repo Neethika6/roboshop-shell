bfba85dc285b3bdfc6f344235821dbd339d46342
Neethika6 <neethikadakoju346@gmail.com>
2025-06-29 11:39:23 +0530

    ROBOSHOP

--- roboshop.sh
+++ roboshop.sh
@@ -4,9 +4,9 @@ AMI_ID="ami-09c813fb71547fc4f"
 SG_ID="sg-0235de9a0de70883b"
 ZONE_NAME="devopshyn.fun"
 ZONE_ID="Z09794961ZIKJQCHWFUWN"
-PACKAGES=( "mongodb" "mysql" "redis" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+#PACKAGES=( "mongodb" "mysql" "redis" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
 
-for package in ${PACKAGES[@]}
+for package in $@
 do
   INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-0235de9a0de70883b --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$package}]" --query 'Instances[0].InstanceId' --output text)
   if [ $package != "frontend" ]
