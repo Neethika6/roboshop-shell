2f4c75c707222c2b1044cd1954f8a73dde674511
Neethika6 <neethikadakoju346@gmail.com>
2025-06-29 12:56:32 +0530

    ROBOSHOP

--- frontend.sh
+++ frontend.sh
@@ -49,7 +49,7 @@ VALIDATE $? "Starting NGINX"
 rm -rf /usr/share/nginx/html/*
 VALIDATE $? "Removing default file in html folder"
 
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 VALIDATE $? "Unzipping files is done"
